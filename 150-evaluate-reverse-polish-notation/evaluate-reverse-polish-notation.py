0d9c8a0a415d4741d92c6a4628dae5c2554dd0f7
Venkata Akhil Bhamidipati <128762376+akhil906@users.noreply.github.com>
2025-01-02 15:45:31 -0500

    Time: 3 ms (65.98%) | Memory: 19.1 MB (40.46%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,28 +1,61 @@
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+
+#         stack = []
+
+#         def eval(a,b,op):
+#             if op == "+":
+#                 return a+b
+#             elif op == "-":
+#                 return a-b
+#             elif op == "*":
+#                 return a*b
+#             elif op == "/":
+#                 return a/b
+        
+        
+        
+#         for i in tokens:
+#             if i == '+' or  i == '-' or  i == '*' or  i == '/':
+#                 b = stack.pop()
+#                 a = stack.pop()
+
+#                 stack.append(eval(int(a),int(b),i))
+#             else:
+#                 stack.append(i)
+        
+#         return int(stack[-1])
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
 
         stack = []
 
-        def eval(a,b,op):
-            if op == "+":
-                return a+b
-            elif op == "-":
-                return a-b
-            elif op == "*":
-                return a*b
-            elif op == "/":
-                return a/b
-        
-        
-        
         for i in tokens:
-            if i == '+' or  i == '-' or  i == '*' or  i == '/':
+            if i == '+':
                 b = stack.pop()
                 a = stack.pop()
+                stack.append(int(a)+int(b))
 
-                stack.append(eval(int(a),int(b),i))
+            elif i == '-':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a)-int(b))
+            
+            elif i == '*':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a)*int(b))
+
+            elif i == '/':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a)/int(b))
+            
             else:
                 stack.append(i)
+            
         
+
         return int(stack[-1])
         
